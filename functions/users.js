963914ca34b03f3e7b70b492192e6f8040293147
Ho Ren Sen <horensen@live.com>
2020-06-25 17:39:15 +0800

    Add sample user

--- functions/users.js
+++ functions/users.js
@@ -1,7 +1,8 @@
 const _users = [
   {"id": "4226201987", "email": "someone@gmail.com", "mobile": "97875077"},
   {"id": "5819949129", "email": "user@gmail.com", "mobile": "85071839"},
-  {"id": "7562282624", "email": "tester@gmail.com", "mobile": "83276688"}
+  {"id": "7562282624", "email": "tester@gmail.com", "mobile": "83276688"},
+  {"id": "3475034321", "email": "briantan@gmail.com", "mobile": "94799974"}
 ];
 
 const getLastTwoDigitsOfMobile = email => {
